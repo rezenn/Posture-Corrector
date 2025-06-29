1450d083c53e6794f1e38990675ea29b365a9d93
Subham Adhikari <subhamadhikari20@gmail.com>
2025-06-29 14:42:20 +0545

    Setting Up Server

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+export default app; 
+
