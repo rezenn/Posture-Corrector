bed53a022003d86afd927bdcc5a8963c675c59c7
Subham Adhikari <subhamadhikari20@gmail.com>
2025-06-29 15:26:15 +0545

    Setting Up controllers, models, routes, .env

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import router from "./routes/UserRoute";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,8 +13,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.use(cors());
+app.use('/api/users', router);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port .................... ${PORT}`);
 });
 export default app; 
 
