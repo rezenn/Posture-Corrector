c4b575dcf6323bbaf360aca65c192a401e4432a6
SajeenaMahotra <149761433+SajeenaMahotra@users.noreply.github.com>
2025-06-30 17:34:37 +0545

    Add Login form

--- frontend/upryt_frontend/src/App.tsx
+++ frontend/upryt_frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import Register from './pages/Register.tsx'
 import Homepage from './pages/Homepage.tsx'
+import Login from './pages/Login.tsx'
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
        
         <Route path='/' index element={<Homepage/>}/>
         <Route path='/register'element={<Register/>}/>
+        <Route path='/login'element={<Login/>}/>
        
 
       </Routes>
