d621dbb0ce4719ea7b891bfc6217296c4c87eaff
Sanskriti Khanal <sanskritikhanal@Sanskritis-MacBook-Air.local>
2025-06-30 15:43:40 +0545

    Register form

---
+++ frontend/upryt_frontend/src/pages/Register.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import WidthWrapper from '../components/WidthWrapper'
+
+const Register = () => {
+  return (
+    <WidthWrapper>
+
+<div className='flex'>
+    <div className='md:w-1/2'>left</div>
+    <div className='md:w-1/2'>right</div>
+        </div>
+    </WidthWrapper>
+  )
+}
+
+export default Register
