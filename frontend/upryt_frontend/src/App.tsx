5928530035036e84856302da641292d5dc2b55a1
SajeenaMahotra <149761433+SajeenaMahotra@users.noreply.github.com>
2025-07-02 12:11:39 +0545

    Resolve merge conflicts

--- frontend/upryt_frontend/src/App.tsx
+++ frontend/upryt_frontend/src/App.tsx
@@ -4,18 +4,9 @@ import Register from './pages/Register.tsx'
 import Homepage from './pages/Homepage.tsx'
 import Login from './pages/Login.tsx'
 import ForgotPassword from './pages/forgotpassword.tsx'
-<<<<<<< HEAD
-=======
 import OtpForm from './pages/otpform.tsx'
 import MainLayout from './components/MainLayout.tsx'
-=======
-<<<<<<< HEAD
-import ForgotPassword from './pages/forgotpassword.tsx'
-=======
->>>>>>> 447e4c075acc178bff2968092fa0b0b8711573c7
 import { Toaster } from 'sonner'
-
->>>>>>> 4a0eb85e25573af89d2fbd2b6541dbd6d52cc0fe
 
 const App = () => {
   return (
