1b1a3308067571f346bf12221716695ae8888985
rezen <rezen.khadgi@gmail.com>
2025-08-04 13:42:34 +0545

    Daily summary and feedback added using NLP

---
+++ posture/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+        self.feedback_response = ttk.Label(
+            self.feedback_panel,
+            text="NLP system initializing...",
+            wraplength=300
+        )
+        self.feedback_response.pack(fill=X)
